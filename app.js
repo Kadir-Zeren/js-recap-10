cc0450d199e66a652333d6fad66e4df255698461
kadir-zeren <k4dzeren@gmail.com>
2023-11-23 09:32:25 +0300

    v1

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("first");
+const mayişlar = [5500, 8000, 6500, 9000, 10000, 15000, 25000];
+const toplamMayiş = mayişlar.reduce((toplam, deger) => toplam + deger, 0);
+console.log("TOPLAM MAYIŞ:", toplamMayiş);
+
+const zamliMaasToplami = mayişlar
+  .filter((m) => m >= 6000 && m <= 10000)
+  .map((m) => m * 1.1)
+  .reduce((t, m) => t + m);
+console.log("ZAMLI MAASLAR:", zamliMaasToplami);
