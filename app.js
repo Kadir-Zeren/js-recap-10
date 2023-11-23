d4191000b4bd6c685465456f83ea3aa9a2b51390
kadir-zeren <k4dzeren@gmail.com>
2023-11-23 10:35:19 +0300

    v4

--- app.js
+++ app.js
@@ -8,18 +8,42 @@
 //   .reduce((t, m) => t + m);
 // console.log("ZAMLI MAASLAR:", zamliMaasToplami);
 
-const ages = [18, 22, 78, 34, 78, 79, 15];
-const check = ages.every((age) => age >= 18);
-check ? console.log("All ages are over 18") : console.log("Sum are under 18");
+// const ages = [18, 22, 78, 34, 78, 79, 15];
+// const check = ages.every((age) => age >= 18);
+// check ? console.log("All ages are over 18") : console.log("Sum are under 18");
 
-const over80 = ages.some((a) => a >= 80);
-console.log(over80);
+// const over80 = ages.some((a) => a >= 80);
+// console.log(over80);
 
-const ageBigThan30First = ages.find((age) => age >= 30);
-const ageBigThan30Last = ages.findLast((age) => age >= 30);
-console.log(ageBigThan30First, ageBigThan30Last);
+// const ageBigThan30First = ages.find((age) => age >= 30);
+// const ageBigThan30Last = ages.findLast((age) => age >= 30);
+// console.log(ageBigThan30First, ageBigThan30Last);
 
-console.log(ages.find((age) => age >= 80));
+// console.log(ages.find((age) => age >= 80));
 
-const foundIndex = ages.findIndex((age) => age > 18);
-console.log(foundIndex);
+// const foundIndex = ages.findIndex((age) => age > 18);
+// console.log(foundIndex);
+
+const car = {
+  brand: "Mercedes",
+  model: 2020,
+  engine: 2000,
+  speed: 5,
+  color: ["pink", "black", "blue", "green"],
+};
+console.log(car);
+console.log(car.brand);
+console.log(car.color[0]);
+console.log(car.color);
+car.color.forEach((c) => console.log(c));
+
+car.speed = 7;
+car.color = "Morcivert";
+car.class = "E";
+console.log(car);
+
+console.log("ENINE:", car["engine"]);
+const myKey = prompt(
+  "Araba ile ilgili neyi ogrenmek istersiniz: speed,color,engine..."
+);
+alert(car[myKey]);
